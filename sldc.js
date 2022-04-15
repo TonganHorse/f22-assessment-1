f8e8447ff7b8e61b5b1458f2f7ce299c047e6098
tuli kinikini <tuli.kinikini@gmail.com>
2022-04-15 10:18:17 -0600

    sdlc finished

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfReq = "This is the second step, in which you define what perimeters are to be met. You would come up with a high level more specific plan and exactly what exactly is needed in the product."
+const design = "This is the third step, here you will make an outline of what code you are going to need to include as well as a more comprehensive list of what requirements will need to be met."
+const implementation = "This is the fourth step, here you will actually that you learned through the the earlier processes and put it in code."
+const testingAndIntegration = "This is the fifth step, you will take the implementations that you made and you will test for quality assurance."
+const Maintain = "This is the final step, this is where further into the future you can continue to update the code the was integrated and update the code essentially."
