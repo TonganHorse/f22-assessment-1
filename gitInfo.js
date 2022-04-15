b5695cf219947bf3cb8bafc7c13ef26a5e4dd684
tuli kinikini <tuli.kinikini@gmail.com>
2022-04-15 10:58:27 -0600

    finished git cheat sheet

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = "Git is a version control system used to track and update lines of code."
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = "Github is a webpage that allows you to store your code onto the cloud. Repositories are stored on someone else's servers."
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+let gitInitDefinition = "Git init is what is used to initialize the process of tracking files in a projection"
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+let gitCloneDefinition = "Git clone is used to target a specific repo and copying that repo."
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+let gitStatusDefinition = "Git status allows you to see which files are being tracked as well as which ones are not."
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -51,7 +51,8 @@
     Create another variable called 'gitAddCode'.  
     It should be a string containing the code to add all files.
 */
-
+let gitAddDefinition = "git add is used to add recent changes that you have made to the project being tracked."
+let gitAddCode = "git add ."
 //CODE HERE
 
 //////////////////PROBLEM 7////////////////////
@@ -64,7 +65,8 @@
 */
 
 //CODE HERE
-
+let gitCommitDefinition = "Git commit is used to sort of finalize changes that you have made to the folder and files to actually track them."
+let gitCommitCode = "git commit -m"
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +75,4 @@
 */
 
 //CODE HERE
+let gitPushDefinition = "Git push takes your committed and added changes to your code and pushes them to Github."
